8a7364d558b90cb22d765fa279034454c5ec557b
Speykious <speykious@gmail.com>
2024-05-31 13:17:53 +0200

    Finally, Zig 0.12

--- build.zig
+++ build.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 const zcc = @import("compile_commands");
 
-pub fn build(b: *std.build.Builder) void {
-    var targets = std.ArrayList(*std.Build.CompileStep).init(b.allocator);
+pub fn build(b: *std.Build) void {
+    var targets = std.ArrayList(*std.Build.Step.Compile).init(b.allocator);
 
     const target = b.standardTargetOptions(.{});
     const mode = b.standardOptimizeOption(.{});
@@ -11,6 +11,7 @@ pub fn build(b: *std.build.Builder) void {
         "-std=c11", "-pedantic", // C11
         "-Wall", "-Werror", // errors as warnings
         "-Wno-missing-field-initializers", // additional warnings
+        "-I./include",
         "-g", // debug
         "-fsanitize=address", // ASAN
     };
@@ -21,12 +22,14 @@ pub fn build(b: *std.build.Builder) void {
         .optimize = mode,
         .link_libc = true,
     });
-    c4_core.force_pic = true;
-    c4_core.addCSourceFiles(&.{
-        "src/core/alloc/arena.c",
-        "src/core/math.c",
-        "src/core/os.c",
-    }, &generalCompilerOptions);
+    c4_core.addCSourceFiles(.{
+        .files = &.{
+            "src/core/alloc/arena.c",
+            "src/core/math.c",
+            "src/core/os.c",
+        },
+        .flags = &generalCompilerOptions,
+    });
     targets.append(c4_core) catch @panic("OOM");
 
     const c4_app = b.addStaticLibrary(.{
@@ -35,13 +38,15 @@ pub fn build(b: *std.build.Builder) void {
         .optimize = mode,
         .link_libc = true,
     });
-    c4_app.force_pic = true;
     c4_app.linkSystemLibrary("X11");
-    c4_app.addCSourceFiles(&.{
-        "src/app/x11/x11.c",
-        "src/app/x11/x11_keycode.c",
-        "src/app/keycode.c",
-    }, &generalCompilerOptions);
+    c4_app.addCSourceFiles(.{
+        .files = &.{
+            "src/app/x11/x11.c",
+            "src/app/x11/x11_keycode.c",
+            "src/app/keycode.c",
+        },
+        .flags = &generalCompilerOptions,
+    });
     targets.append(c4_app) catch @panic("OOM");
 
     const c4_audio = b.addStaticLibrary(.{
@@ -50,11 +55,11 @@ pub fn build(b: *std.build.Builder) void {
         .optimize = mode,
         .link_libc = true,
     });
-    c4_audio.force_pic = true;
     c4_audio.linkSystemLibrary("asound");
-    c4_audio.addCSourceFiles(&.{
-        "src/audio/audio.c",
-    }, &generalCompilerOptions);
+    c4_audio.addCSourceFiles(.{
+        .files = &.{"src/audio/audio.c"},
+        .flags = &generalCompilerOptions,
+    });
     targets.append(c4_audio) catch @panic("OOM");
 
     const c4 = b.addExecutable(.{
@@ -67,14 +72,17 @@ pub fn build(b: *std.build.Builder) void {
     c4.linkLibrary(c4_core);
     c4.linkLibrary(c4_app);
     c4.linkLibrary(c4_audio);
-    c4.addCSourceFiles(&.{
-        "src/main.c",
-    }, &generalCompilerOptions);
+    c4.addCSourceFiles(.{
+        .files = &.{"src/main.c"},
+        .flags = &generalCompilerOptions,
+    });
     targets.append(c4) catch @panic("OOM");
 
     const run_step = b.addRunArtifact(c4);
     b.step("run", "Run for your fucking life.").dependOn(&run_step.step);
     b.installArtifact(c4);
 
-    zcc.createStep(b, "cdb", targets.toOwnedSlice() catch @panic("OOM"));
+    zcc.createStep(b, "cdb", .{
+        .targets = targets.toOwnedSlice() catch @panic("OOM"),
+    });
 }
